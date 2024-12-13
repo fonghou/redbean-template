9998dd67789ec9d287b8357c0af21ea08384bbc4
user <user@local.domain>
2024-12-13 17:46:06 -0500

    update

--- .nvim.lua
+++ .nvim.lua
@@ -2,4 +2,4 @@ vim.g["conjure#filetype#fennel"] = "conjure.client.fennel.stdio"
 vim.g["conjure#client#fennel#stdio#command"] = "make repl"
 vim.g["conjure#client#sql#stdio#command"] = "psql postgres"
 vim.g["conjure#client#sql#stdio#prompt_pattern"] = "postgres=> "
-print("loaded .nvimrc.lua")
+print("loaded .nvim.lua")
